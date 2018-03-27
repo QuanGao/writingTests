ccc160a3029572727a06c98bf505cb81db397575
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-03-26 22:07:38 -0500

    persistence function

--- test/test.js
+++ test/test.js
@@ -1,26 +1,49 @@
 const expect = require("chai").expect;
 
-function findOdd(A) {
-
-    var obj = A.reduce(function (a, b) {
-        a[b] ? a[b]=a[b]*(-1): a[b] = -1
-            return a
-        }, {}
-    )
-
-    for(var i in obj){
-        if(obj[i] === -1){
-            return parseInt(i)
-        }
+var counter = 0;
+function persistence(num) {
+    if (num.toString().length > 1) {
+        counter++
+        var nextNum = num.toString().split("").reduce((a, b) => {
+            return (+a) * (+b)
+        })
+        return persistence(nextNum)
+    } else {
+        return counter;
     }
 }
 
 
-describe("findOdd", function(){
-    it("should find the integer that appears odd number of times from an array of integers", function(){
-        expect(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])).to.equal(5)
+describe("persistence", function(){
+    it("should find the the number of operations need to reduce the number to single digits", function(){
+        expect(persistence(999)).to.equal(4)
     })
 })
+
+
+
+
+
+
+
+// function findOdd(A) {
+//     var obj = A.reduce(function (a, b) {
+//         a[b] ? a[b]=a[b]*(-1): a[b] = -1
+//             return a
+//         }, {}
+//     )
+
+//     for(var i in obj){
+//         if(obj[i] === -1){
+//             return parseInt(i)
+//         }
+//     }
+// }
+// describe("findOdd", function(){
+//     it("should find the integer that appears odd number of times from an array of integers", function(){
+//         expect(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])).to.equal(5)
+//     })
+// })
 
 
 
