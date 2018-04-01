0efb7c9e7a9cfaec202deed895992301ed94580d
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-04-01 16:24:29 -0500

    connect4

--- test/test.js
+++ test/test.js
@@ -1,92 +1,133 @@
 const expect = require("chai").expect;
+const log = console.log
+//-------------------------------------------------------------------------------------
+const moves1 = [
+    {p: 'R', x: 0, y: 0},
+    {p: 'Y', x: 100, y: 100},
+    {p: 'R', x: 1, y: 0},
+    {p: 'Y', x: 99, y: 100},
+    {p: 'R', x: 2, y: 0},
+    {p: 'Y', x: 98, y: 100},
+    {p: 'R', x: 3, y: 0},
+    {p: 'Y', x: 96, y: 100}
+  ];
+  
+  const moves2 = [
+    {p: 'R', x: 15, y: 15},
+    {p: 'Y', x: 10, y: 10},
+    {p: 'Y', x: 10, y: 11},
+    {p: 'Y', x: 10, y: 12},
+    {p: 'Y', x: 10, y: 13},
+    {p: 'R', x: 15, y: 16},
+    {p: 'R', x: 16, y: 15},
+    {p: 'R', x: 16, y: 16}
+  ];
+  
+  const moves3 = [
+    {p: 'G', x: 0, y: 0},
+    {p: 'Y', x: 0, y: 1},
+    {p: 'G', x: 1, y: 1},
+    {p: 'Y', x: 1, y: 2},
+    {p: 'G', x: 2, y: 2},
+    {p: 'Y', x: 2, y: 3},
+    {p: 'G', x: -1, y: -1},
+    {p: 'Y', x: 3, y: 3}
+  ];
+  
+  const moves4 = [
+    {p: 'A', x: 50, y: -50},
+    {p: 'B', x: 49, y: -50},
+    {p: 'C', x: 48, y: -50},
+    {p: 'D', x: 50, y: -49},
+    {p: 'A', x: 51, y: -50},
+    {p: 'B', x: 49, y: -49},
+    {p: 'C', x: 48, y: -51},
+    {p: 'D', x: 51, y: -49}
+  ]
+  
+  const moves5 = [
+    {p: 'P', x: 0, y: 1},
+    {p: 'P', x: 0, y: -1},
+    {p: 'P', x: 0, y: -2},
+    {p: 'P', x: 0, y: -3},
+    {p: 'E', x: 0, y: 0},
+    {p: 'E', x: 1, y: -1},
+    {p: 'E', x: 2, y: -2},
+    {p: 'E', x: 3, y: -3}
+  ]
+const connectFour = moves => {
 
-function toCamelCase(str){
-    return str.replace(/([-_]+)([a-zA-Z])/g, function(match, p1, p2, index, string){
-        console.log({match, p1, p2, index, string})
-        return p2.toUpperCase()
-    }) 
+    var players = moves.map(item=>item.p).reduce((a,b)=>{
+        a[b]? a[b]++:a[b]=1;
+        return a;
+    }, {})
+
+    var potentials = Object.keys(players).filter(item=>players[item]>=4);
+    
+    if(potentials.length>0){
+        for(var i=0; i<potentials.length; i++){
+            var pmoves = moves.filter(item=>item.p === potentials[i]).sort((a,b)=>{
+                return +a.x - +b.x === 0? +a.y - +b.y : +a.x - +b.x;
+            });
+            while(pmoves.length>3){   
+                var xStrike = 1, yStrike = 1,dStrikePlus = 1,dStrikeMinus = 1;
+                var records = pmoves.reduce(function(a,b){
+                    if(a.x+1===b.x && a.y+1 === b.y){
+                        dStrikePlus++;
+                        return b;
+                    } else if (a.x+1===b.x && a.y-1 === b.y){
+                        dStrikeMinus++;
+                        return b;
+                    }else if (a.x ===b.x && a.y+1 === b.y){
+                        yStrike++;
+                        return b;
+                    } else if (a.x+1===b.x && a.y === b.y){
+                        xStrike++;
+                        return b;
+                    } else {
+                        return a;
+                    }
+                })
+
+                if(xStrike>=4 || yStrike>=4 || dStrikePlus>=4 || dStrikeMinus>=4){
+                    return potentials[i]
+                }
+                pmoves.shift();           
+            }       
+        }
+        return null;
+    } else {
+        return null;
+    }
 }
 
-console.log(toCamelCase("the_-_-----stealth-____warrior"));
+describe("Tests", function() {
+  it("4 in a row", function() {
+    expect(connectFour(moves1)).to.equals('R');
+  });
+  it("4 in a column", function(){
+    expect(connectFour(moves2)).to.equals('Y');
+  });
+  it("4 in diagonal", function(){
+    expect(connectFour(moves3)).to.equals('G');
+  });
+  it("no winner", function(){
+    expect(connectFour(moves4)).to.equals(null);
+  });
+  it("4 in diagonal", function(){
+    expect(connectFour(moves5)).to.equals('E');
+  });
+});
 
 //-------------------------------------------------------------------------------------
-// const connectFour = moves => {
-//     //find out players and number of moves
-//   const players = moves.fil
+// function toCamelCase(str){
+//     return str.replace(/([-_]+)([a-zA-Z])/g, function(match, p1, p2, index, string){
+//         console.log({match, p1, p2, index, string})
+//         return p2.toUpperCase()
+//     }) 
 // }
 
-// const moves1 = [
-//   {p: 'R', x: 0, y: 0},
-//   {p: 'Y', x: 100, y: 100},
-//   {p: 'R', x: 1, y: 0},
-//   {p: 'Y', x: 99, y: 100},
-//   {p: 'R', x: 2, y: 0},
-//   {p: 'Y', x: 98, y: 100},
-//   {p: 'R', x: 3, y: 0},
-//   {p: 'Y', x: 96, y: 100}
-// ];
-
-// const moves2 = [
-//   {p: 'R', x: 15, y: 15},
-//   {p: 'Y', x: 10, y: 10},
-//   {p: 'Y', x: 10, y: 11},
-//   {p: 'Y', x: 10, y: 12},
-//   {p: 'Y', x: 10, y: 13},
-//   {p: 'R', x: 15, y: 16},
-//   {p: 'R', x: 16, y: 15},
-//   {p: 'R', x: 16, y: 16}
-// ];
-
-// const moves3 = [
-//   {p: 'G', x: 0, y: 0},
-//   {p: 'Y', x: 0, y: 1},
-//   {p: 'G', x: 1, y: 1},
-//   {p: 'Y', x: 1, y: 2},
-//   {p: 'G', x: 2, y: 2},
-//   {p: 'Y', x: 2, y: 3},
-//   {p: 'G', x: -1, y: -1},
-//   {p: 'Y', x: 3, y: 3}
-// ];
-
-// const moves4 = [
-//   {p: 'A', x: 50, y: -50},
-//   {p: 'B', x: 49, y: -50},
-//   {p: 'C', x: 48, y: -50},
-//   {p: 'D', x: 50, y: -49},
-//   {p: 'A', x: 51, y: -50},
-//   {p: 'B', x: 49, y: -49},
-//   {p: 'C', x: 48, y: -51},
-//   {p: 'D', x: 51, y: -49}
-// ]
-
-// const moves5 = [
-//   {p: 'P', x: 0, y: 1},
-//   {p: 'P', x: 0, y: -1},
-//   {p: 'P', x: 0, y: -2},
-//   {p: 'P', x: 0, y: -3},
-//   {p: 'E', x: 0, y: 0},
-//   {p: 'E', x: 1, y: -1},
-//   {p: 'E', x: 2, y: -2},
-//   {p: 'E', x: 3, y: -3}
-// ]
-
-// describe("Tests", function() {
-//   it("4 in a row", function() {
-//     Test.assertDeepEquals(connectFour(moves1), 'R', 'Should check 4 in a row');
-//   });
-//   it("4 in a column", function(){
-//     Test.assertDeepEquals(connectFour(moves2), 'Y', 'Should check 4 in a column')
-//   });
-//   it("4 in diagonal", function(){
-//     Test.assertDeepEquals(connectFour(moves3), 'G', 'Should check 4 in diagonal')
-//   });
-//   it("no winner", function(){
-//     Test.assertDeepEquals(connectFour(moves4), null, 'Should check if there\'s no winner')
-//   });
-//   it("4 in diagonal", function(){
-//     Test.assertDeepEquals(connectFour(moves5), 'E', 'Should check 4 in diagonal')
-//   });
-// });
+// console.log(toCamelCase("the_-_-----stealth-____warrior"));
 
 //-------------------------------------------------------------------------------------
 // function EightQueens(strArr) { 
@@ -155,7 +196,6 @@ console.log(toCamelCase("the_-_-----stealth-____warrior"));
 //     })
 // })
 
-
 //-------------------------------------------------------------------------------------
 // function solution1(number){
 //     var sum = 0;
@@ -168,7 +208,6 @@ console.log(toCamelCase("the_-_-----stealth-____warrior"));
 //   }
 
 // function solution2(number){
-
 //     function power (number, base) {
 //         var i = 1,res = 0;    
 //         while(base*i < number) {          
